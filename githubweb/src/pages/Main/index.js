7264863a43bad42e67b1709b5c7db205e85f1a55
Shilton <shilton.sistemas@gmail.com>
2019-11-11 23:59:34 -0300

    Implementando listagem de repositorios, loading e localStorage

--- githubweb/src/pages/Main/index.js
+++ githubweb/src/pages/Main/index.js
@@ -1,22 +1,93 @@
-import React from 'react';
-import { FaGithubAlt, FaPlus } from 'react-icons/fa';
-import { Container, Form, SubmitButton } from './styles';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/state-in-constructor */
+import React, { Component } from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Container, Form, SubmitButton, List } from './styles';
+import api from '../../services/api';
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositórios
-      </h1>
+export default class Main extends Component {
+  state = {
+    novoNomeRepo: '',
+    repositorios: [],
+    loading: false,
+  };
 
-      <Form onSubmit={() => {}}>
-        <input type="text" placeholder="Adicionar repositório" />
+  componentDidMount() {
+    const repositorios = localStorage.getItem('repositorios');
 
-        <SubmitButton>
-          <FaPlus color="#FFF" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+    if (repositorios) {
+      this.setState({
+        repositorios: JSON.parse(repositorios),
+      });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { repositorios } = this.state;
+
+    if (prevState.repositorios === repositorios) {
+      localStorage.setItem('repositorios', JSON.stringify(repositorios));
+    }
+  }
+
+  handleInputChange = e => {
+    this.setState({ novoNomeRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { novoNomeRepo, repositorios } = this.state;
+
+    const response = await api.get(`repos/${novoNomeRepo}`);
+    const data = {
+      name: response.data.full_name,
+    };
+
+    this.setState({
+      repositorios: [...repositorios, data],
+      novoNomeRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { novoNomeRepo, loading, repositorios } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório"
+            value={novoNomeRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositorios.map(repositorio => (
+            <li key={repositorio.name}>
+              <span>{repositorio.name}</span>
+              <a href="">mais detalhes</a>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
 }
--- githubweb/src/pages/Main/styles.js
+++ githubweb/src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 
 export const Container = styled.div`
   max-width: 700px;
@@ -34,9 +34,19 @@ export const Form = styled.form`
   }
 `;
 
-export const SubmitButton = styled.button.attrs({
+// rotação icone loading
+const rotacao = keyframes`
+from {
+transform: rotacao(0deg);
+}
+to {
+  transform: rotacao(360deg);
+}`;
+
+export const SubmitButton = styled.button.attrs(props => ({
   type: 'submit',
-})`
+  disabled: props.loading,
+}))`
   background: #7159c1;
   border: 0;
   padding: 0 15px;
@@ -47,4 +57,42 @@ export const SubmitButton = styled.button.attrs({
   display: flex;
   justify-content: center;
   align-items: center;
+
+  /* manipulando loadin button */
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+
+  /* aplicando rotacao ao icon do button */
+  ${props =>
+    props.loading &&
+    css`
+      svg {
+        animation: ${rotacao} 2s linear infinite;
+      }
+    `}
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  margin-top: 30px;
+
+  li {
+    padding: 15px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+
+    /* aplica estilo so após primeiro item  */
+    & + li {
+      border-top: 1px solid #eee;
+    }
+
+    a {
+      color: #7159c1;
+      text-decoration: none;
+    }
+  }
 `;
